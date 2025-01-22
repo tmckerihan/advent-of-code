fcf20bc017b0fe5074614d81d79be3ed631e7526
tmckerihan <tylermckerihan@gmail.com>
2025-01-22 18:23:31 +1100

    bug: need to skip over skipped_idx for next check still

--- 2024/2/main.go
+++ 2024/2/main.go
@@ -37,6 +37,54 @@ func txtTotList(path string) [][]int {
 	return number_lists
 }
 
+func isAscending(slice []int, allow_skip bool) (bool, int) {
+	skipped_idx := -1
+	for i := range len(slice) - 1 {
+		// ascending check
+		if slice[i] < slice[i+1] {
+			continue
+		} else if allow_skip && (i < len(slice)-2) && (slice[i] < slice[i+2]) {
+			// only allow one skip
+			if skipped_idx != -1 {
+				return false, skipped_idx
+			}
+			skipped_idx = i + 1
+			continue
+		} else {
+			return false, skipped_idx
+		}
+	}
+	return true, skipped_idx
+}
+
+func isDescending(slice []int, allow_skip bool) (bool, int) {
+	skipped_idx := -1
+	for i := range len(slice) - 1 {
+		// descending check
+		if slice[i] > slice[i+1] {
+			continue
+		} else if allow_skip && (i < len(slice)-2) && (slice[i] > slice[i+2]) {
+			// only allow one skip
+			if skipped_idx != -1 {
+				return false, skipped_idx
+			}
+			skipped_idx = i + 1
+			continue
+		} else {
+			return false, skipped_idx
+		}
+	}
+	return true, skipped_idx
+}
+
+func isSortedNew(slice []int, allow_skip bool) (bool, int) {
+	ascendingCheck, skipped_idx := isAscending(slice, allow_skip)
+	if ascendingCheck {
+		return true, skipped_idx
+	}
+	return isDescending(slice, allow_skip)
+}
+
 func isSorted(slice []int) bool {
 	isAscending := slices.IsSortedFunc(slice, func(a, b int) int {
 		if a < b {
@@ -66,28 +114,57 @@ func isSorted(slice []int) bool {
 	return false
 }
 
-func isSafeDistances(number_list []int) bool {
+func isSafeDistances(number_list []int, allow_skip_idx int) bool {
+	skipped_idx := -1
 	for i := range len(number_list) - 1 {
 		distance := math.Abs(float64(number_list[i] - number_list[i+1]))
 		if distance < 1 || distance > 3 {
+			// if skip allowed
+			if (allow_skip_idx != -1 || allow_skip_idx == i+1) && i < len(number_list)-2 {
+				// allow single skip
+				if skipped_idx != -1 {
+					return false
+				}
+				skipped_idx = i + 1
+				distance = math.Abs(float64(number_list[i] - number_list[i+2]))
+				if distance < 1 || distance > 3 {
+					return false
+				}
+				return true
+			}
 			return false
 		}
 	}
 	return true
 }
 
-func isSafe(number_list []int) bool {
-	if !isSorted(number_list) || !isSafeDistances(number_list) {
+func isSafe(number_list []int, allow_skip bool) bool {
+	listIsSorted, skipped_idx := isSortedNew(number_list, allow_skip)
+	if !listIsSorted {
 		return false
 	}
-	return true
-
+	// allow a single rule skip
+	listIsSafeDistances := isSafeDistances(number_list, skipped_idx)
+	if skipped_idx != -1 && !listIsSafeDistances {
+		fmt.Println(number_list, skipped_idx)
+	}
+	return listIsSafeDistances
 }
 
 func puzzle1(number_lists [][]int) {
 	safe_count := 0
 	for _, number_list := range number_lists {
-		if isSafe(number_list) {
+		if isSafe(number_list, false) {
+			safe_count += 1
+		}
+	}
+	fmt.Println(safe_count)
+}
+
+func puzzle2(number_lists [][]int) {
+	safe_count := 0
+	for _, number_list := range number_lists {
+		if isSafe(number_list, true) {
 			safe_count += 1
 		}
 	}
@@ -97,4 +174,5 @@ func puzzle1(number_lists [][]int) {
 func main() {
 	number_lists := txtTotList("./input.txt")
 	puzzle1(number_lists)
+	puzzle2(number_lists)
 }
